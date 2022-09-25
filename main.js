757d324c6944d38cdbf8250f4a334bf901ed040a
Franticcboy <83899259+Franticcboy@users.noreply.github.com>
2022-09-25 11:51:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera')
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+    });
+
+}
+console.log('ml5.version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Mbe8zxbq-/model.json',modelloadin);
+function modelloadin(){
+    console.log('modelloadin')
+}
